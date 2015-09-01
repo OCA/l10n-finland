86397f3f2c0740264dbf63a2e31d7ed7449a32c0
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-09-01 12:37:37 +0300

    Remove redundant description

--- l10n_fi_liikekirjuri/__openerp__.py
+++ l10n_fi_liikekirjuri/__openerp__.py
@@ -32,20 +32,6 @@
         'base_vat',
         'base_iban'
     ],
-    'description': """
-Odoo Finnish Localization (l10n_fi_liikekirjuri)
-===================================
-
-A Finnish chart of accounts and tax localization for Odoo.
-Based on 'Liikekirjurit' accounts chart version 7.0
-(http://www.kirjurituote.fi/tuotteet/liikekirjuri/)
-
-This is the comprehensive version of the accounts chart,
-and should be sufficient for all industries and company types.
-
-The number of accounts is approx. 950
-The number of headers is approx. 300
-""",
     'data': [
         'data/account.account.type.csv',
         'data/account.account.template.csv',
