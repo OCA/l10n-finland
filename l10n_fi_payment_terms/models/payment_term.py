a86bfacaf7861ff42012e2bdd0eb18c01b180c1b
Jarmo Kortetjärvi <jarmokortetjarvi@users.noreply.github.com>
2017-05-05 08:20:19 +0300

    Remove unnecessary comments

--- l10n_fi_payment_terms/models/payment_term.py
+++ l10n_fi_payment_terms/models/payment_term.py
@@ -24,15 +24,3 @@ class PaymentTerm(models.Model):
 
     sequence = fields.Integer('Sequence')
     code = fields.Char('Unique code')
-
-    # 3. Default methods
-
-    # 4. Compute and search fields, in the same order that fields declaration
-
-    # 5. Constraints and onchanges
-
-    # 6. CRUD methods
-
-    # 7. Action methods
-
-    # 8. Business methods
