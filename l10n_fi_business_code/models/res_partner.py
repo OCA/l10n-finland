6c77a5a94d4d6ba416780283d040806cd6f59e74
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-09-29 14:18:09 +0300

    Add a partner_identification dependent field

--- l10n_fi_business_code/models/res_partner.py
+++ l10n_fi_business_code/models/res_partner.py
@@ -8,4 +8,15 @@ from odoo import models, fields
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    business_id = fields.Char('Business id')
+    business_id = fields.Char(
+        string='Business ID',
+        compute=lambda s: s._compute_identification(
+            'business_id', 'business_id',
+        ),
+        inverse=lambda s: s._inverse_identification(
+            'business_id', 'business_id',
+        ),
+        search=lambda s, *a: s._search_identification(
+            'business_id', *a
+        ),
+    )
