afeb84c39effd4e3286c82e93f8b53832c8ef7e3
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-09-29 15:08:02 +0300

    Add a new module for business id validation

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Oy Tawasta OS Technologies Ltd.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import res_partner_id_category
+from . import res_partner_id_number
