d64ec38267f5b53bbdb63b1fceebb09636576df8
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-10-24 16:44:50 +0300

    Fix formatting issues

--- l10n_fi_payment_terms/models/payment_term.py
+++ l10n_fi_payment_terms/models/payment_term.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
-from openerp import fields, models
+from odoo import fields, models
 
 
 class PaymentTerm(models.Model):
 
-    # 1. Private attributes
     _inherit = 'account.payment.term'
-
-    # 2. Fields declaration
     _order = 'sequence, name'
 
     sequence = fields.Integer('Sequence')
