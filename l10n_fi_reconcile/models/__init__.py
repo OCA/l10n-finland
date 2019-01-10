08db8a8e571c2aa5fe284c07f359963ccf2ebc85
Mikko Närjänen <mikko.narjanen@avoin.systems>
2019-01-10 14:21:18 +0200

    [FEAT] auto reconclile by payment reference only

--- l10n_fi_reconcile/models/__init__.py
+++ l10n_fi_reconcile/models/__init__.py
@@ -1,1 +1,4 @@
-from . import account_move, account_bank_statement
+from . import account_move
+from . import account_bank_statement
+from . import res_company
+from . import res_config
--- l10n_fi_reconcile/models/account_bank_statement.py
+++ l10n_fi_reconcile/models/account_bank_statement.py
@@ -21,7 +21,8 @@
 
 import logging
 
-from odoo import models
+from odoo import api, models
+from odoo.exceptions import UserError
 from odoo.tools import float_round, float_repr
 
 _logger = logging.getLogger(__name__)
@@ -29,6 +30,109 @@ _logger = logging.getLogger(__name__)
 
 class AccountBankStatementLine(models.Model):
     _inherit = 'account.bank.statement.line'
+
+    @api.multi
+    def auto_reconcile(self):
+        """
+        Try to automatically reconcile the statement.line
+        return the counterpart journal entry/ies if the automatic reconciliation succeeded, False otherwise.
+        Copied with modifications from
+        https://github.com/odoo/odoo/blob/11.0/addons/account/models/account_bank_statement.py#L669
+        """
+        if self.company_id.auto_reconcile_method != 'finnish':
+            return super(AccountBankStatementLine, self).auto_reconcile()
+
+        _logger.debug('>>> auto_reconcile({})'.format(self))
+
+        self.ensure_one()
+        match_recs = self.env['account.move.line']
+
+        amount = self.amount_currency or self.amount
+        company_currency = self.journal_id.company_id.currency_id
+        st_line_currency = self.currency_id or self.journal_id.currency_id
+        currency = (st_line_currency and st_line_currency != company_currency) and st_line_currency.id or False
+        precision = st_line_currency and st_line_currency.decimal_places or company_currency.decimal_places
+        params = {'company_id': self.env.user.company_id.id,
+                  'account_payable_receivable': (
+                      self.journal_id.default_credit_account_id.id, self.journal_id.default_debit_account_id.id),
+                  'amount': float_round(amount, precision_digits=precision),
+                  'partner_id': self.partner_id.id,
+                  'ref': self.name,
+                  }
+        _logger.debug(params)
+        field = currency and 'amount_residual_currency' or 'amount_residual'
+        liquidity_field = currency and 'amount_currency' or amount > 0 and 'debit' or 'credit'
+        # Look for structured communication match
+        if self.name:
+            sql_query = self._get_common_sql_query() + \
+                        " AND aml.ref = %(ref)s AND (" + field + \
+                        " = %(amount)s OR (acc.internal_type = 'liquidity' AND " + liquidity_field + \
+                        " = %(amount)s)) ORDER BY date_maturity asc, aml.id asc"
+            self.env.cr.execute(sql_query, params)
+            match_recs = self.env.cr.dictfetchall()
+            _logger.debug(sql_query)
+            _logger.debug(match_recs)
+            if len(match_recs) > 1:
+                return False
+
+        # DIFF: Look for matching structured communication in payment_reference field
+        if self.name and not match_recs:
+            # sql_query = self._get_common_sql_query() + " AND aml.payment_reference = %(ref)s"
+            sql_query = self._get_common_sql_query() + \
+                        " AND aml.payment_reference = %(ref)s AND (" + field + \
+                        " = %(amount)s OR (acc.internal_type = 'liquidity' AND " + liquidity_field + \
+                        " = %(amount)s)) ORDER BY date_maturity asc, aml.id asc"
+            self.env.cr.execute(sql_query, params)
+            match_recs = self.env.cr.dictfetchall()
+            _logger.debug(sql_query)
+            _logger.debug(match_recs)
+            if len(match_recs) > 1:
+                return False
+
+        # DIFF: Do not try to match anything if structured communication match fails
+        # Look for a single move line with the same partner, the same amount
+        # if not match_recs:
+        #     if self.partner_id:
+        #         sql_query = self._get_common_sql_query() + \
+        #                     " AND (" + field + " = %(amount)s OR (acc.internal_type = 'liquidity' AND " + \
+        #                     liquidity_field + " = %(amount)s)) ORDER BY date_maturity asc, aml.id asc"
+        #         self.env.cr.execute(sql_query, params)
+        #         match_recs = self.env.cr.dictfetchall()
+        #         if len(match_recs) > 1:
+        #             return False
+
+        if not match_recs:
+            return False
+
+        match_recs = self.env['account.move.line'].browse([aml.get('id') for aml in match_recs])
+        # Now reconcile
+        counterpart_aml_dicts = []
+        payment_aml_rec = self.env['account.move.line']
+        for aml in match_recs:
+            if aml.account_id.internal_type == 'liquidity':
+                payment_aml_rec = (payment_aml_rec | aml)
+            else:
+                amount = aml.currency_id and aml.amount_residual_currency or aml.amount_residual
+                counterpart_aml_dicts.append({
+                    'name': aml.name if aml.name != '/' else aml.move_id.name,
+                    'debit': amount < 0 and -amount or 0,
+                    'credit': amount > 0 and amount or 0,
+                    'move_line': aml
+                })
+
+        try:
+            with self._cr.savepoint():
+                counterpart = self.process_reconciliation(counterpart_aml_dicts=counterpart_aml_dicts,
+                                                          payment_aml_rec=payment_aml_rec)
+            return counterpart
+        except UserError:
+            # A configuration / business logic error that makes it impossible to auto-reconcile should not be raised
+            # since automatic reconciliation is just an amenity and the user will get the same exception when manually
+            # reconciling. Other types of exception are (hopefully) programmation errors and should cause a stacktrace.
+            self.invalidate_cache()
+            self.env['account.move'].invalidate_cache()
+            self.env['account.move.line'].invalidate_cache()
+            return False
 
     def get_reconciliation_proposition(self, excluded_ids=None):
         # pylint: disable=sql-injection
