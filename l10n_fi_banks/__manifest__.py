5ce3a73bf333363aa5620fbd0fe679e71557e5ed
Daniels Andersons <daniels.andersons@avoin.systems>
2020-01-22 11:37:01 +0200

    [IMP] l10n_fi_banks: black, isort

--- l10n_fi_banks/__manifest__.py
+++ l10n_fi_banks/__manifest__.py
@@ -19,19 +19,15 @@
 ##############################################################################
 
 {
-    'name': 'Finnish Banks',
-    'summary': 'Finnish banks and their addresses',
-    'version': '11.0.1.0.0',
-    'category': 'Localization',
-    'website': 'https://odoo-community.org/',
-    'author': 'Oy Tawasta Technologies Ltd., Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'application': False,
-    'installable': True,
-    'depends': [
-        'base',
-    ],
-    'data': [
-        'data/res_bank.xml',
-    ],
+    "name": "Finnish Banks",
+    "summary": "Finnish banks and their addresses",
+    "version": "11.0.1.0.0",
+    "category": "Localization",
+    "website": "https://odoo-community.org/",
+    "author": "Oy Tawasta Technologies Ltd., Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["base"],
+    "data": ["data/res_bank.xml"],
 }
