bb2d85ee8d3f4c1c64e343aa8402066eab5c2994
Miku Laitinen <miku.laitinen@avoin.systems>
2021-05-11 15:48:45 +0300

    [MIG] Migrated l10n_fi_payment_terms from 12.0 to 13.0

--- l10n_fi_payment_terms/models/payment_term.py
+++ l10n_fi_payment_terms/models/payment_term.py
@@ -3,8 +3,8 @@ from odoo import fields, models
 
 class PaymentTerm(models.Model):
 
-    _inherit = 'account.payment.term'
-    _order = 'sequence, name'
+    _inherit = "account.payment.term"
+    _order = "sequence, name"
 
-    sequence = fields.Integer('Sequence')
-    code = fields.Char('Unique code')
+    sequence = fields.Integer("Sequence")
+    code = fields.Char("Unique code")
