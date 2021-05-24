1a5c1fe624f7f97f420b280b2161dbd0c76a5b85
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2021-05-24 10:46:14 +0300

    [MIG] l10n_fi_business_code: Migration to 14.0

--- l10n_fi_business_code/migrations/13.0.1.0.0/pre-migration.py
+++
@@ -1,9 +0,0 @@
-from openupgradelib import openupgrade
-
-
-@openupgrade.migrate()
-def migrate(env, version):
-    # Rename business_id to business_code
-    openupgrade.rename_fields(
-        env, [("res.partner", "res_partner", "business_id", "business_code",)],
-    )
