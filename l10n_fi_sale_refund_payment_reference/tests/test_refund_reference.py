c0c1d353b2c9cf3430e3ee68d6b6466c013a479c
Nedas Zilinskas <nedas.zilinskas@gmail.com>
2021-12-17 18:59:13 +0200

    [MIG] l10n_fi_sale_refund_payment_reference: Migration to 15.0

--- l10n_fi_sale_refund_payment_reference/tests/test_refund_reference.py
+++ l10n_fi_sale_refund_payment_reference/tests/test_refund_reference.py
@@ -1,6 +1,6 @@
 from odoo.tests import tagged
 
-from odoo.addons.account.tests.account_test_savepoint import AccountTestInvoicingCommon
+from odoo.addons.account.tests.common import AccountTestInvoicingCommon
 
 
 @tagged("post_install", "-at_install")
@@ -10,17 +10,17 @@ class TestRefundGeneratesReference(AccountTestInvoicingCommon):
         # super will skip this test if l10n_generic_coa is not installed
         super().setUpClass()
 
-        cls.invoice = cls.init_invoice("out_refund")
+        cls.invoice = cls.init_invoice("out_refund", products=cls.product_a)
 
     def test_get_reference_odoo_refund(self):
-        self.assertFalse(self.invoice.invoice_payment_ref)
+        self.assertFalse(self.invoice.payment_reference)
         self.invoice.journal_id.invoice_reference_model = "odoo"
-        self.invoice.post()
-        self.assertTrue(self.invoice.invoice_payment_ref)
+        self.invoice.action_post()
+        self.assertTrue(self.invoice.payment_reference)
 
     def test_get_reference_odoo_partner(self):
-        self.assertFalse(self.invoice.invoice_payment_ref)
+        self.assertFalse(self.invoice.payment_reference)
         self.invoice.journal_id.invoice_reference_type = "partner"
         self.invoice.journal_id.invoice_reference_model = "odoo"
-        self.invoice.post()
-        self.assertTrue(self.invoice.invoice_payment_ref)
+        self.invoice.action_post()
+        self.assertTrue(self.invoice.payment_reference)
