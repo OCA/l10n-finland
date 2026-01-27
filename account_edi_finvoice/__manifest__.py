5d1e9d511c2b5acebc6b99c940cd9a182b8b9725
Jussi Lehto <jussi.lehto@cetmix.com>
2026-01-27 13:05:46 +0200

    manual merge of https://github.com/tawasta/connector-apix/commit/c0d29385a818120ba579aed49e8ad3793218b7e3

--- account_edi_finvoice/__manifest__.py
+++ account_edi_finvoice/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Import/Export invoices as Finvoice",
     "summary": "Import/Export Finvoice 3.0 invoices",
-    "version": "18.0.1.0.3",
+    "version": "18.0.1.0.4",
     "category": "Accounting",
     "website": "https://gitlab.com/tawasta/odoo/edi",
     "author": "Futural",
